0951fbf4d846fe10af9ce5858af1a3fab0bb6f90
Billy Ceskavich <bceskavich@gmail.com>
2014-04-05 14:27:48 -0400

    Ready for heroku deploy.

--- config.py
+++ config.py
@@ -6,7 +6,11 @@ SECRET_KEY = 't0h1e0s1k0i1e0s1a0w1a0i1t0'
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Path of the database file
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
 # Path where we store the migration data files
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
